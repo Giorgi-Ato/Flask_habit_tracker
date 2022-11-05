1a2fc440fe54fbace76f38a59cbe3c2f739994e2
Giorgi Seperidze <oniani.giorgi.97@gmail.com>
2022-11-06 01:06:24 +0400

    usind mongoDb for reading and saving habits

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from routes import pages
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import os
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages)
+    client = MongoClient(os.environ.get('MONGODB_URI'))
+
+    app.db = client.get_default_database()
 
     return app
